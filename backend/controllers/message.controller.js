2e1ec6dd4ef6e33c0a53412812b5be5a4ca2d4ae
Kavya Naik <kavyanaik2112@gmail.com>
2025-02-26 15:06:41 +0530

    messages and conversations

---
+++ backend/controllers/message.controller.js
@@ -0,0 +1,54 @@
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { message } = req.body;
+    const { id: recieverId } = req.params;
+    const { _id: senderId } = req.user;
+
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, recieverId] },
+    });
+
+    if (!conversation) {
+      conversation = await new Conversation({
+        participants: [senderId, recieverId],
+      });
+    }
+
+    const newMessage = await new Message({
+      senderId,
+      recieverId,
+      message,
+    });
+
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
+    }
+    await conversation.save();
+    await newMessage.save();
+    res
+      .status(201)
+      .json({ success: true, message: "Message sent successfully" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export const getMessage = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages");
+
+    if (!conversation) return res.status(200).json([]);
+
+    res.status(201).json({ messages: conversation.messages });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 dotenv.config();
 const app = express();
 const appPort = process.env.PORT || 5000;
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("HELLO WORLD!!");
 });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(appPort, () => {
   connectToMongoDB();
