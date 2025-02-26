3c4680b1fe6e3db8448e440d023386e17369ac30
Kavya Naik <kavyanaik2112@gmail.com>
2025-02-27 00:04:05 +0530

    added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/home/Home.jsx";
+import Login from "./pages/login/Login.jsx";
+import Signup from "./pages/signup/Signup.jsx";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const authUser = false;
+  const appRoutes = createBrowserRouter([
+    {
+      path: "/",
+      element: authUser ? <Home /> : <Navigate to={"/login"} />,
+    },
+    {
+      path: "/login",
+      element: authUser ? <Navigate to={"/"} /> : <Login />,
+    },
+    {
+      path: "/signup",
+      element: authUser ? <Navigate to={"/"} /> : <Signup />,
+    },
+  ]);
 
-export default App
+  return <RouterProvider router={appRoutes}></RouterProvider>;
+};
+
+export default App;
